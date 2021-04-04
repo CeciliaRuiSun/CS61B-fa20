d25fdf01020c9430f984a9a07bda60b5456e9689
Cecilia <sunrui7337@gmail.com>
2021-04-03 22:06:13 -0400

    initialize Hexagon

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,33 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 30;
+
+    public static void main(String[] args) {
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        // draws the world to the screen
+        ter.renderFrame(world);
+    }
+
+    private void addHexagon(TETile[][] world, int x, int size){
+        int xbegin = x;
+        int ybegin = HEIGHT - 1 - 2 * size;
+        for(int i = 0; i < size; i ++){
+            for(int j = 0; j < size; j++) {
+                world[xbegin - i ][ybegin + i] = Tileset.WALL;
+            }
+        }
+    }
+
 }
