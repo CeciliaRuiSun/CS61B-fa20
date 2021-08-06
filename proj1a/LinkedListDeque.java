59aeddcc4dbe4efe654da39bf5f588ce10220fee
CeciliaRuiSun <44910072+CeciliaRuiSun@users.noreply.github.com>
2021-08-05 21:30:34 -0700

    Circular Sentinel for Double Ended Queue

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,129 +1,103 @@
-public class LinkedListDeque <Item>{
+public class LinkedListDeque<T> {
+    private int size = 0;
+    private Node Sentinal;
 
-    private class DoubleNode{
-        public DoubleNode pre;
-        public Item item;
-        public DoubleNode next;
+    private class Node{
+        Node pre;
+        T item;
+        Node next;
 
-        public DoubleNode(DoubleNode a, Item b, DoubleNode c){
-            pre = a;
-            item = b;
-            next = c;
+        public Node(T t, Node p, Node n){
+            item = t;
+            pre = p;
+            next = n;
         }
+
     }
 
-    private DoubleNode sentinelHead;
-    private DoubleNode sentinelTail;
-    private int size;
-
-    //create an empty linkedlist
     public LinkedListDeque(){
-        sentinelHead = new DoubleNode(null,null,null);
-        sentinelTail = new DoubleNode(null,null,null);
-        sentinelHead.next = sentinelTail;
-        sentinelHead.pre = sentinelTail;
-        sentinelTail.next = sentinelHead;
-        sentinelTail.pre = sentinelHead;
-        size = 0;
+        Sentinal = new Node(null,null,null);
+        Sentinal.next = Sentinal;
+        Sentinal.pre = Sentinal;
     }
 
-    public LinkedListDeque(Item item){
-        sentinelHead = new DoubleNode(null,null,null);
-        sentinelHead.next = new DoubleNode(sentinelHead,item,null);
-        sentinelTail = new DoubleNode(null,null,null);
-        sentinelHead.next.next = sentinelTail;
-        sentinelTail.pre = sentinelHead.next;
-        sentinelTail.next = sentinelHead;
-        sentinelHead.pre = sentinelTail;
-        size = 1;
+    public void addFirst(T item){
+        Sentinal = new Node(null,null,null);
+        Sentinal.next = new Node(item,Sentinal,Sentinal);
+        Sentinal.pre = Sentinal.next;
+        size += 1;
     }
 
-    public Boolean isEmpty(){
-        if(size == 0) {
+    public void addLast(T item){
+        Sentinal.pre.next = new Node(item,Sentinal.pre,Sentinal);
+        Sentinal.pre = Sentinal.pre.next;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        if(size == 0){
             return true;
-        }
-        return false;
-
-    }
-
-    public void addFirst(Item item){
-        sentinelHead.next = new DoubleNode(sentinelHead,item,sentinelHead.next);
-        sentinelHead.next.next.pre = sentinelHead.next;
-        size += 1;
-    }
-
-    public void addLast(Item item){
-        sentinelTail.pre = new DoubleNode(sentinelTail.pre,item,sentinelTail);
-        sentinelTail.pre.pre.next = sentinelTail.pre;
-        size += 1;
-    }
-
-    public Item removeFirst(){
-        Item res;
-        if(sentinelHead.next != sentinelTail){
-            res = sentinelHead.next.item;
         } else{
-            res = null;
+            return false;
         }
-        sentinelHead.next.next.pre = sentinelHead;
-        sentinelHead.next = sentinelHead.next.next;
-        size -= 1;
-
-        return res;
-    }
-
-    public Item removeLast(){
-        Item res;
-        if(sentinelTail.pre != sentinelHead){
-            res =  sentinelTail.pre.item;
-        } else{
-            res =  null;
-        }
-        sentinelTail.pre.pre.next = sentinelTail;
-        sentinelTail.pre = sentinelTail.pre.pre;
-        size -= 1;
-
-        return res;
     }
 
     public int size(){
         return size;
     }
 
-    public Item get(int index){
-        if(index < size) {
-            DoubleNode P;
-            P = sentinelHead;
-            while (index-- > 0) {
-                P = P.next;
-            }
-            return P.next.item;
-        }
-        return null;
-    }
-
-    public Item getRecursive(int index){
-        if(index < size) {
-            return getHelper(index).item;
-        }
-        return null;
-    }
-
-    public DoubleNode getHelper(int n){
-        //recursion method to get node
-        if (n == 0){
-            return sentinelHead.next;
-        }
-        return getHelper(n - 1).next;
-    }
-
     public void printDeque(){
-        DoubleNode P;
-        P = sentinelHead;
-        while(P.next != sentinelTail){
-            System.out.println(P.next.item);
-            P = P.next;
+        int count = size;
+        Node p = Sentinal.next;
+        while(count != 0){
+            System.out.println(p.item);
+            p = p.next;
+            count --;
         }
     }
 
+    public T removeFirst(){
+        T item = Sentinal.next.item;
+        Sentinal.next.next = Sentinal;
+        Sentinal.next = Sentinal.next.next;
+        size -= 1;
+        return item;
+    }
+
+    public T removeLast(){
+        T item = Sentinal.pre.item;
+        Sentinal.pre.pre.next = Sentinal;
+        Sentinal.pre = Sentinal.pre.pre;
+        size -= 1;
+        return item;
+    }
+
+    public T get(int index){
+        Node n = Sentinal.next;
+        if(index >= size){
+            return null;
+        }
+        while(index != 0){
+            n = n.next;
+            index --;
+        }
+
+        return n.item;
+    }
+
+    public T getRecursive(int index){
+        if(index >= size || index <= 0){
+            return null;
+        }
+        Node n = Sentinal.next;
+        return helper(index,n);
+    }
+
+    private T helper(int index, Node n){
+        if(index == 0){
+            return n.item;
+        }
+
+        return helper(index --, n.next);
+    }
 }
